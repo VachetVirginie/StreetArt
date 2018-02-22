b85ed3dfc6bdb040d86a5350ac5cb919399d60a4
Vachet <vachetvirginie@gmail.com>
2018-02-22 11:53:47 +0100

    Add files via upload

---
+++ responsivemap.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+
+function initialize() {
+
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+// Resize stuff...
+google.maps.event.addDomListener(window, "resize", function() {
+   var center = map.getCenter();
+   google.maps.event.trigger(map, "resize");
+   map.setCenter(center); 
+});
+
+</script>
